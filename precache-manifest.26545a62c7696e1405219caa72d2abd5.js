45221abe93fb5c5cf8ee3fe11d29a70d8960ff39
Shivaram Vasudevan <shivaramv23@gmail.com>
2019-02-15 01:28:07 +0530

    Updates

---
+++ precache-manifest.26545a62c7696e1405219caa72d2abd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb5f8c8a9f8a58b2961e",
+    "url": "/example-create-react-app/static/css/main.30f0e8e4.chunk.css"
+  },
+  {
+    "revision": "cb5f8c8a9f8a58b2961e",
+    "url": "/example-create-react-app/static/js/main.cb5f8c8a.chunk.js"
+  },
+  {
+    "revision": "938af90eb3b06f55ae84",
+    "url": "/example-create-react-app/static/js/runtime~main.938af90e.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/example-create-react-app/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/example-create-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e664937db70c9a9c9bf8f0ef79eb89d8",
+    "url": "/example-create-react-app/index.html"
+  }
+];
